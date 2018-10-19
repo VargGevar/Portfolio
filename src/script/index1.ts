ec67f4a5198b7ddc13b5729ba08bd4b31af586bc
VargGevar <36130257+VargGevar@users.noreply.github.com>
2018-10-19 23:29:34 +0300

    add some final blocks

--- src/script/index1.ts
+++ src/script/index1.ts
@@ -1,32 +1,32 @@
-(function () {
+// (function () {
 
-  const slider = document.querySelector('.header_main_slider');
+//   const slider = document.querySelector('.header_main_slider');
 
-  let slideSwitch = function () {
+//   let slideSwitch = function () {
 
-    let index = 0;
+//     let index = 0;
 
-    let slide = slider.querySelectorAll('*')
+//     let slide = slider.querySelectorAll('*')
 
-    for (index = 0; index < slide.length; index++) {
-      setInterval(function () {
-        for (let i = 0; i < slide.length; i++) {
-          let el = slide[i];
-          el.classList.remove('active')
-          if (el.classList.contains('active')) {
+//     for (index = 0; index < slide.length; index++) {
+//       setInterval(function () {
+//         for (let i = 0; i < slide.length; i++) {
+//           let el = slide[i];
+//           el.classList.remove('active')
+//           if (el.classList.contains('active')) {
             
-          } else if (i = index) {
-            el.classList.add('active')
-          }
-        }
-      }, 3000)
-    }
+//           } else if (i = index) {
+//             el.classList.add('active')
+//           }
+//         }
+//       }, 3000)
+//     }
 
-  }
+//   }
 
-  slideSwitch();
+//   slideSwitch();
 
 
-})()
+// })()
 
 
